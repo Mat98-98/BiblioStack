7e60bc488f2707b7772cbe379a30a0f12c28039c
Tuo Nome <tuo.email@gmail.com>
2026-08-15 15:35:38 +0200

    style
    style: Sistemato l'altezza del div che mostra il titolo e l'autore nelle card delle opere

--- frontend/src/components/common/works/cards/WorkCard.jsx
+++ frontend/src/components/common/works/cards/WorkCard.jsx
@@ -4,10 +4,9 @@ function formatAuthors(authors) {
     return authors.map((a) => `${a.firstName} ${a.lastName}`).join(", ")
 }
 
-// @todo Texture per la copertina finta
 function FakeCover({ title, authors }) {
     return (
-        <div className="w-full h-full bg-gradient-to-br from-zinc-800 to-zinc-900 text-white p-4 flex flex-col justify-between">
+        <div className="w-full h-full bg-linear-to-br from-zinc-800 to-zinc-900 text-white p-4 flex flex-col justify-between">
 
             {/* Center */}
             <div className="flex-1 flex items-center">
@@ -42,7 +41,7 @@ export default function WorkCard({ work }) {
                 )}
             </div>
 
-            <div className="flex flex-col gap-1 p-3">
+            <div className="flex flex-col gap-1 p-3 h-22 justify-start">
                 <span className="font-semibold text-sm leading-tight line-clamp-2">
                     {work.title}
                 </span>
@@ -54,42 +53,3 @@ export default function WorkCard({ work }) {
         </Link>
     )
 }
-
-/*
-export default function WorkCard({ work }) {
-    return (
-        <div className="group flex flex-col rounded-md border border border-border bg-card overflow-hidden hover:shadow-md transition-shadow h-70 w-40">
-
-
-            <div className="aspect-2/3 bg-secondary/50 flex items-center justify-center overflow-hidden">
-
-                {work.coverUrl ? (
-                    <img
-                        src={work.coverUrl}
-                        alt={work.title}
-                        className="w-full h-full object-fill group-hover:scale-105 transition-transform duration-300 cursor-pointer"
-                    />
-                ) : (
-                    <FakeCover
-                        title={work.title}
-                        authors={work.authors}
-                    />
-                )}
-
-            </div>
-
-
-            <div className="flex flex-col gap-1 p-3">
-                <span className="font-semibold text-sm leading-tight line-clamp-2">
-                    {work.title}
-                </span>
-
-                <span className="text-xs text-muted-foreground line-clamp-1">
-                    {formatAuthors(work.authors)}
-                </span>
-            </div>
-
-        </div>
-    )
-}
- */
